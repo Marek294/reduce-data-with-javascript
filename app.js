39d9efff74647a5d3feefde281f6bbb97eb70906
Marek294 <Markra5835@gmail.com>
2018-05-14 17:18:03 +0200

    safely inspect nested objects with reduce

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-function increment(input) { return input + 1 };
-function decrement(input) { return input - 1};
-function double(input) { return input * 2 };
-function halve(input) { return input / 2 };
-
-var initial_value = 1;
-
-var pipeline = [
-    increment,
-    increment,
-    increment,
-    double,
-    increment,
-    increment,
-    halve
-]
-
-var final_value = pipeline.reduceRight((acc,fn) => {
-    return fn(acc);
-}, initial_value)
-
-console.log(final_value);
+var luke = {
+    name: "luke skywalker",
+    jedi: true,
+    parents: {
+      father: {
+        jedi: true
+      },
+      mother: {
+        jedi: false
+      }
+    }
+  }
+  
+  var han = {
+    name: "han solo",
+    jedi: false,
+    parents: {
+      father: {
+        jedi: false
+      },
+      mother: {
+        jedi: false
+      }
+    }
+  }
+  
+  var anakin = {
+    name: "anakin skywalker",
+    jedi: true,
+    parents: {
+      mother: {
+        jedi: false
+      }
+    }
+  }
+  
+  var characters = [luke, han, anakin];
+  
+  function fatherWasJedi(character) {
+    var path = "parents.father.jedi";
+    return path.split(".").reduce(function(obj, field) {
+      if (obj) {
+        return obj[field];
+      }
+  
+      return false;
+    }, character);
+  }
+  
+  characters.forEach(function(character) {
+    console.log(character.name + "'s father was a jedi:", fatherWasJedi(character)) 
+  });
