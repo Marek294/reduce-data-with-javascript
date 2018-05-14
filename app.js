39b488aa97e10512b428f1cd3b25844071716a7d
Marek294 <Markra5835@gmail.com>
2018-05-14 16:41:34 +0200

    Use the Optional Reduce Arguments

--- app.js
+++ app.js
@@ -1,52 +1,12 @@
-var data = [1, 2, 3];
-
-var doubled = data.reduce((acc, value) => {
-    acc.push(value * 2);
-
-    return acc;
-}, []);
-
-var doubleMapped = data.map( item => {
-    return item * 2;
-})
-
-var data2 = [1, 2, 3, 4, 5, 6];
-var evens = data2.reduce((acc,value) => {
-    if(value % 2 === 0) {
-        acc.push(value);
+function reducer(acc, value, index, array) {
+    var intermediaryValue = acc + value;
+    if(index === array.length-1) {
+        return intermediaryValue/array.length;
     }
-
-    return acc;
-}, []);
-
-var evenFiltered = data2.filter(item => {
-    return item % 2 === 0;
-})
-
-var filterMapped = data2.filter(value => {
-    return value % 2 === 0;
-}).map(value => {
-    return value * 2;
-})
-
-var bigData = [];
-for(var i = 0; i < 1000000; i++) {
-    bigData[i] = i;
+    return intermediaryValue;
 }
 
-console.time('bigData');
-var filterMappedBigData = bigData.filter(value => {
-    return value % 2 === 0;
-}).map(value => {
-    return value * 2;
-})
-console.timeEnd('bigData');
+var data = [1, 2, 3, 3, 4, 5, 3, 1];
+var mean = data.reduce(reducer, 0);
 
-console.time('bigDataReduce');
-var reducedBigData = bigData.reduce((acc,value) => {
-    if(value % 2 === 0) {
-        acc.push(value*2);
-    }
-    return acc;
-},[])
-console.timeEnd('bigDataReduce');
+console.log(mean);
