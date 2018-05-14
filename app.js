9f2b15e2fa0c74be4ca38122507869cf8314cb02
Marek294 <Markra5835@gmail.com>
2018-05-14 17:10:12 +0200

    Compose Functions with Reduce

--- app.js
+++ app.js
@@ -1,67 +1,22 @@
-var data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+function increment(input) { return input + 1 };
+function decrement(input) { return input - 1};
+function double(input) { return input * 2 };
+function halve(input) { return input / 2 };
 
-var flattenedData = data.reduce((acc, value) => {
-    return acc.concat(value);
-}, []);
+var initial_value = 1;
 
-console.log(flattenedData);
+var pipeline = [
+    increment,
+    increment,
+    increment,
+    double,
+    increment,
+    increment,
+    halve
+]
 
-var input = [
-    {
-      title: "Batman Begins",
-      year: 2005,
-      cast: [
-        "Christian Bale",
-        "Michael Caine",
-        "Liam Neeson",
-        "Katie Holmes",
-        "Gary Oldman",
-        "Cillian Murphy"
-      ]
-    },
-    {
-      title: "The Dark Knight",
-      year: 2008,
-      cast: [
-        "Christian Bale",
-        "Heath Ledger",
-        "Aaron Eckhart",
-        "Michael Caine",
-        "Maggie Gyllenhal",
-        "Gary Oldman",
-        "Morgan Freeman"
-      ]
-    },
-    {
-      title: "The Dark Knight Rises",
-      year: 2012,
-      cast: [
-        "Christian Bale",
-        "Gary Oldman",
-        "Tom Hardy",
-        "Joseph Gordon-Levitt",
-        "Anne Hathaway",
-        "Marion Cotillard",
-        "Morgan Freeman",
-        "Michael Caine"
-      ]
-    }
-  ];
-  
-  var stars = input.reduce(function(acc, value) {
-    value.cast.forEach(function(star) {
-      if (acc.indexOf(star) === -1) {
-        acc.push(star);
-      }
-    });
-  
-    return acc;
-  }, []);
-  
-  var data = [1, 2, 3, 4, "5"];
-  var sum = data.reduceRight(function(acc, value, index) {
-    console.log(index)
-    return acc + value;
-  }, 0);
-  
-  console.log(sum)
+var final_value = pipeline.reduceRight((acc,fn) => {
+    return fn(acc);
+}, initial_value)
+
+console.log(final_value);
